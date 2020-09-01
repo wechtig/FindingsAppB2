23ff7ad08fcf4ee32a40ad97adcbad011b48f675
wechtig <wechtitsch.lukas@gmail.com>
2020-09-01 21:20:00 +0200

    removed downloadtask

--- app/src/main/java/at/fhjoanneum/findingsapp/BL/HttpsClient.java
+++ app/src/main/java/at/fhjoanneum/findingsapp/BL/HttpsClient.java
@@ -5,22 +5,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
-/**
- * General workflow for connections with UrlConnection:
- *
- * The connection object is created by invoking the openConnection method on a URL.
- * The setup parameters and general request properties are manipulated.
- * The actual connection to the remote object is made, using the connect method.
- * The remote object becomes available. The header fields and the contents of the remote object can be accessed.
- *
- */public class HttpsClient {
+public class HttpsClient {
     public String get(URL url) throws IOException {
         InputStream stream = null;
         HttpURLConnection connection = null;
@@ -29,13 +19,8 @@ import javax.net.ssl.HttpsURLConnection;
             connection = (HttpURLConnection) url.openConnection();
             connection.setReadTimeout(3000);
             connection.setConnectTimeout(3000);
-
-            //this is a get request
             connection.setRequestMethod("GET");
-            //indicate that this connection wants to read from the connection
             connection.setDoInput(true);
-
-            //connect and therefore initiate network communication
             connection.connect();
 
             int responseCode = connection.getResponseCode();
@@ -52,100 +37,6 @@ import javax.net.ssl.HttpsURLConnection;
                 }
                 result = total.toString();
             }
-        } finally {
-            if (stream != null) {
-                stream.close();
-            }
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-        return result;
-    }
-
-    public String getList(URL url) throws IOException {
-        InputStream stream = null;
-        HttpURLConnection connection = null;
-        String result = null;
-        try {
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setReadTimeout(3000);
-            connection.setConnectTimeout(3000);
-
-            //this is a get request
-            connection.setRequestMethod("GET");
-            //indicate that this connection wants to read from the connection
-            connection.setDoInput(true);
-
-            //connect and therefore initiate network communication
-            connection.connect();
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode != HttpsURLConnection.HTTP_OK) {
-                throw new IOException("HTTP error code: " + responseCode);
-            }
-            stream = connection.getInputStream();
-
-            if(stream != null) {
-                BufferedReader r = new BufferedReader(new InputStreamReader(stream));
-                StringBuilder total = new StringBuilder();
-                for (String line; (line = r.readLine()) != null; ) {
-                    total.append(line).append('\n');
-                }
-                result = total.toString();
-            }
-        } finally {
-            if (stream != null) {
-                stream.close();
-            }
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-        return result;
-    }
-
-    public String post(URL url, String body) throws IOException {
-        InputStream stream = null;
-        HttpsURLConnection connection = null;
-        String result = null;
-        try {
-            connection = (HttpsURLConnection) url.openConnection();
-            connection.setReadTimeout(3000);
-            connection.setConnectTimeout(3000);
-
-            //this is a post request
-            connection.setRequestMethod("POST");
-            //indicate that this connection wants to read from the connection
-            connection.setDoOutput(true);
-
-            //write request body to connection object
-            OutputStream outputStream = connection.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-8");
-            outputStreamWriter.write(body);
-            outputStreamWriter.flush();
-            outputStreamWriter.close();
-            outputStream.close();
-
-            //connect and therefore initiate network communication
-            connection.connect();
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode != HttpsURLConnection.HTTP_OK) {
-                throw new IOException("HTTP error code: " + responseCode);
-            }
-
-            stream = connection.getInputStream();
-
-            if(stream != null) {
-                BufferedReader r = new BufferedReader(new InputStreamReader(stream));
-                StringBuilder total = new StringBuilder();
-                for (String line; (line = r.readLine()) != null; ) {
-                    total.append(line).append('\n');
-                }
-                result = total.toString();
-            }
-
         } finally {
             if (stream != null) {
                 stream.close();
